eea91386263bfd6dd4f9d4c7c072a1bc5ba0f02d
xuning <asouxuning@163.com>
2018-03-19 15:48:35 +0800

    add data

--- download_group_data.py
+++ download_group_data.py
@@ -1,5 +1,9 @@
+import tushare as ts
 import pandas as pd
 import time
+
+#df = ts.get_concept_classified()
+#df.to_csv('data/hist/concept_classified.csv')
 
 df = pd.read_csv('data/concept_classified.csv', #converters={'code': str})
                  dtype={'code': str})
@@ -9,10 +13,12 @@ l = len(df)
 for i in range(l):
   stock = df.iloc[i]
   code = stock['code']
-  store_path = 'data/h/'+code+'.csv'
+  store_path = 'data/hist/'+code+'.csv'
   print(store_path)
-  data = ts.get_h_data(code,index=True)
-  data.to_csv(store_path)
+  #data = ts.get_h_data(code,index=True)
+  data = ts.get_hist_data(code)
+  if data is not None:
+    data.to_csv(store_path)
   time.sleep(30)  
 
 
